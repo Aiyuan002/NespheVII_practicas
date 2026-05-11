efd80e2a3d63daad4885815a5659b9b3ab29e9b4
xavi2713 <espinorca410@gmail.com>
2026-05-11 18:58:31 +0200

    Puerta secreta update

--- Assets/Scripts/Terrain/PuertaSecreta.cs
+++ Assets/Scripts/Terrain/PuertaSecreta.cs
@@ -1,18 +1,48 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PuertaSecreta : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int hitsToBreak = 3;
+
+    private int currentHits;
+    private Vector3 startPosition;
+
     void Start()
     {
-        gameObject.GetComponent<BoxCollider2D>();
+        startPosition = transform.position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-            gameObject.SetActive(false);
+        if (collision.CompareTag("Projectile"))
+{
+    currentHits++;
+
+    StartCoroutine(ShakeWall());
+
+    Destroy(collision.gameObject);
+
+    if (currentHits >= hitsToBreak)
+    {
+        gameObject.SetActive(false);
+    }
+}
+    }
+
+    IEnumerator ShakeWall()
+    {
+        float shakeTime = 0.15f;
+
+        while (shakeTime > 0)
+        {
+            shakeTime -= Time.deltaTime;
+
+            transform.position = startPosition + (Vector3)Random.insideUnitCircle * 0.05f;
+
+            yield return null;
+        }
+
+        transform.position = startPosition;
     }
 }
